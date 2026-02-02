aa46c507febcc22810acd75caea6b39b4e89889a
lironobel <lironn217@gmail.com>
2026-02-02 15:38:22 +0200

    Final: Fixed Makefile paths and header declarations

--- include/tree_graph.h
+++ include/tree_graph.h
@@ -7,4 +7,7 @@
 // פונקציה לייצוא עץ ההחלטה לקובץ DOT
 void export_tree_to_dot(Node *node, FILE *file, char **feature_names);
 
+// פונקציה לייצוא עץ ההחלטה לקובץ PNG
+void generate_and_open_graph(const char *dot_filename, const char *png_filename);
+
 #endif
